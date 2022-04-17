4ae668b58e1279a7fd65c2d7c09f708d2926c2d2
Gillian Mc Quillan <X00179268@myTUDublin.ie>
2022-04-17 13:02:20 +0100

    Store data methods added to controller (Views = Create, Update and Delete and Details

--- Drippyz/Controllers/StoreController.cs
+++ Drippyz/Controllers/StoreController.cs
@@ -36,5 +36,39 @@ namespace Drippyz.Controllers
             await _service.AddAsync(store);
             return RedirectToAction(nameof(Index));
         }
+        //Get: Stores Details 
+        public async Task<IActionResult> Details(int id)
+        {
+            var storeDetails = await _service.GetByIdAsync(id);
+            if (storeDetails == null) return View("NotFound");
+            return View(storeDetails);
+        }
+        //Get the store details edit Id
+        //Post request after the details are updated
+        public async Task<IActionResult> Edit(int id)
+        {
+            var storeDetails = await _service.GetByIdAsync(id);
+            if (storeDetails == null) return View("NotFound");
+            return View(storeDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Glyph,Name,Description")] Store store)
+        {
+            if (!ModelState.IsValid) return View(store);
+            await _service.AddAsync(store);
+            return RedirectToAction(nameof(Index));
+        }
+        //Store Delete
+        [HttpPost, ActionName("Delete")]
+        //of store exists in database call the delete confirm method
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var storeDetails = await _service.GetByIdAsync(id);
+            if (storeDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- Drippyz/Models/Store.cs
+++ Drippyz/Models/Store.cs
@@ -10,12 +10,16 @@ namespace Drippyz.Models
         public int Id { get; set; }
 
         [Display(Name = "Company Logo")]
+        [Required(ErrorMessage ="Store Logo is required")]
         public string Glyph { get; set; }
 
+
         [Display(Name = "Company Name")]
+        [Required(ErrorMessage = "Store Name is required")]
         public string Name { get; set; }
 
         [Display(Name = "About")]
+        [Required(ErrorMessage = "Store Description is required")]
         public string Description { get; set; }
 
         //Relationship (Store can have a list of products)
